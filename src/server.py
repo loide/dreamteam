2f220fa00c89d22208a6ebbff7ded62885636dae
Loide Mara Verdes <mara.verdes@gmail.com>
2019-10-03 21:49:29 -0400

    Organize the code

--- src/server.py
+++ src/server.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """
 Main module of the server file
 """
@@ -8,9 +10,6 @@ import connexion
 from config import connex_app as app
 
 
-# Create the application instance
-#app = connexion.App(__name__, specification_dir="./")
-
 # Read the swagger.yml file to configure the endpoints
 app.add_api("swagger.yml")
 
@@ -18,11 +17,6 @@ app.add_api("swagger.yml")
 # create a URL route in our application for "/"
 @app.route("/")
 def home():
-    """
-    This function just responds to the browser URL
-    localhost:5000/
-    :return:        the rendered template "home.html"
-    """
     return "Server up and running."
 
 
